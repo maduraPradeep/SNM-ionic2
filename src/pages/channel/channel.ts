df106fbeeb5227cb46f06288e496a9f1817ee60a
Madura <madura@sqrmobile.com>
2016-10-09 23:25:54 +0530

    Implemented search doctor

--- src/pages/channel/channel.ts
+++ src/pages/channel/channel.ts
@@ -1,6 +1,15 @@
 import {Component, ViewChild} from '@angular/core';
 
-import {AlertController, App, ItemSliding, List, ModalController, NavController} from 'ionic-angular';
+import {
+  AlertController,
+  App,
+  ItemSliding,
+  List,
+  ModalController,
+  NavController,
+  ActionSheet,
+  ActionSheetController
+} from 'ionic-angular';
 
 import {ConferenceData} from '../../providers/conference-data';
 import {DoctorService} from '../../providers/doctors-data';
@@ -10,130 +19,165 @@ import {UserData} from '../../providers/user-data';
 
 
 @Component({
-    selector: 'page-schedule',
-    templateUrl: 'channel.html'
+  selector: 'page-schedule',
+  templateUrl: 'channel.html'
 })
 export class SchedulePage {
-    // the list is a child of the schedule page
-    // @ViewChild('scheduleList') gets a reference to the list
-    // with the variable #scheduleList, `read: List` tells it to return
-    // the List and not a reference to the element
-    @ViewChild('scheduleList', {read: List}) scheduleList: List;
+  // the list is a child of the schedule page
+  // @ViewChild('scheduleList') gets a reference to the list
+  // with the variable #scheduleList, `read: List` tells it to return
+  // the List and not a reference to the element
+  @ViewChild('scheduleList', {read: List}) scheduleList: List;
 
-    dayIndex = 0;
-    queryText = '';
-    segment = 'all';
-    excludeTracks = [];
-    shownSessions: any = [];
-    groups = [];
-    doctors = [];
+  dayIndex = 0;
+  queryText = '';
+  segment = 'all';
+  excludeTracks = [];
+  shownSessions: any = [];
+  groups = [];
+  doctors = [];
+  actionSheet: ActionSheet;
 
-    constructor(public alertCtrl: AlertController,
-                public app: App,
-                public modalCtrl: ModalController,
-                public navCtrl: NavController,
-                public confData: ConferenceData,
-                public doctorService: DoctorService,
-                public user: UserData) {
+  constructor(public alertCtrl: AlertController,
+              public app: App,
+              public modalCtrl: ModalController,
+              public navCtrl: NavController,
+              public confData: ConferenceData,
+              public doctorService: DoctorService,
+              public user: UserData, public actionSheetCtrl: ActionSheetController) {
 
-    }
+  }
 
-    ionViewDidEnter() {
-        this.app.setTitle('Channel');
-    }
+  ionViewDidEnter() {
+    this.app.setTitle('Channel');
+  }
 
-    ngAfterViewInit() {
+  ngAfterViewInit() {
+    this.updateSchedule();
+  }
+
+  updateSchedule() {
+    // Close any open sliding items when the schedule updates
+    this.scheduleList && this.scheduleList.closeSlidingItems();
+
+    this.confData.getTimeline(this.dayIndex, this.queryText, this.excludeTracks, this.segment).then(data => {
+      this.shownSessions = data.shownSessions;
+      this.groups = data.groups;
+    });
+    /*this.doctorService.load().then(data => {
+     console.log(data);
+     return this.doctors=data;
+
+     });*/
+    this.doctorService.search(this.queryText).then(data=> {
+      console.log(data);
+      return this.doctors = data;
+    });
+  };
+
+  search() {
+    this.doctors = this.doctorService.search(this.queryText);
+    /*if (this.queryText&& this.queryText !== '') {
+     this.doctors = this.doctors.filter((item) => {
+     return (item.name.toLowerCase().indexOf(this.queryText.toLowerCase()) > -1);
+     })
+     }else{
+     this.doctors=this.doctorService.load();
+     }*/
+  }
+
+  presentFilter() {
+    let modal = this.modalCtrl.create(ScheduleFilterPage, this.excludeTracks);
+    modal.present();
+
+    modal.onDidDismiss((data: any[]) => {
+      if (data) {
+        this.excludeTracks = data;
         this.updateSchedule();
+      }
+    });
+
+  }
+
+  goToSessionDetail(sessionData) {
+    // go to the session detail page
+    // and pass in the session data
+    this.navCtrl.push(SessionDetailPage, sessionData);
+  }
+
+  addFavorite(slidingItem: ItemSliding, sessionData) {
+
+    if (this.user.hasFavorite(sessionData.name)) {
+      // woops, they already favorited it! What shall we do!?
+      // prompt them to remove it
+      this.removeFavorite(slidingItem, sessionData, 'Favorite already added');
+    } else {
+      // remember this session as a user favorite
+      this.user.addFavorite(sessionData.name);
+
+      // create an alert instance
+      let alert = this.alertCtrl.create({
+        title: 'Favorite Added',
+        buttons: [{
+          text: 'OK',
+          handler: () => {
+            // close the sliding item
+            slidingItem.close();
+          }
+        }]
+      });
+      // now present the alert on top of all other content
+      alert.present();
     }
 
-    updateSchedule() {
-        // Close any open sliding items when the schedule updates
-        this.scheduleList && this.scheduleList.closeSlidingItems();
+  }
 
-        this.confData.getTimeline(this.dayIndex, this.queryText, this.excludeTracks, this.segment).then(data => {
-            this.shownSessions = data.shownSessions;
-            this.groups = data.groups;
-        });
-        this.doctorService.load().then(data => {
-            console.log(data);
-            return this.doctors=data;
+  removeFavorite(slidingItem: ItemSliding, sessionData, title) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      message: 'Would you like to remove this session from your favorites?',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            // they clicked the cancel button, do not remove the session
+            // close the sliding item and hide the option buttons
+            slidingItem.close();
+          }
+        },
+        {
+          text: 'Remove',
+          handler: () => {
+            // they want to remove this session from their favorites
+            this.user.removeFavorite(sessionData.name);
+            this.updateSchedule();
 
-        });
-    }
-
-    presentFilter() {
-        let modal = this.modalCtrl.create(ScheduleFilterPage, this.excludeTracks);
-        modal.present();
-
-        modal.onDidDismiss((data: any[]) => {
-            if (data) {
-                this.excludeTracks = data;
-                this.updateSchedule();
-            }
-        });
-
-    }
-
-    goToSessionDetail(sessionData) {
-        // go to the session detail page
-        // and pass in the session data
-        this.navCtrl.push(SessionDetailPage, sessionData);
-    }
-
-    addFavorite(slidingItem: ItemSliding, sessionData) {
-
-        if (this.user.hasFavorite(sessionData.name)) {
-            // woops, they already favorited it! What shall we do!?
-            // prompt them to remove it
-            this.removeFavorite(slidingItem, sessionData, 'Favorite already added');
-        } else {
-            // remember this session as a user favorite
-            this.user.addFavorite(sessionData.name);
-
-            // create an alert instance
-            let alert = this.alertCtrl.create({
-                title: 'Favorite Added',
-                buttons: [{
-                    text: 'OK',
-                    handler: () => {
-                        // close the sliding item
-                        slidingItem.close();
-                    }
-                }]
-            });
-            // now present the alert on top of all other content
-            alert.present();
+            // close the sliding item and hide the option buttons
+            slidingItem.close();
+          }
         }
+      ]
+    });
+    // now present the alert on top of all other content
+    alert.present();
+  }
 
-    }
+  openContact(doctor) {
+    //let mode = this.config.get('mode');
 
-    removeFavorite(slidingItem: ItemSliding, sessionData, title) {
-        let alert = this.alertCtrl.create({
-            title: title,
-            message: 'Would you like to remove this session from your favorites?',
-            buttons: [
-                {
-                    text: 'Cancel',
-                    handler: () => {
-                        // they clicked the cancel button, do not remove the session
-                        // close the sliding item and hide the option buttons
-                        slidingItem.close();
-                    }
-                },
-                {
-                    text: 'Remove',
-                    handler: () => {
-                        // they want to remove this session from their favorites
-                        this.user.removeFavorite(sessionData.name);
-                        this.updateSchedule();
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Contact with ' + doctor.name,
+      buttons: [
+        {
+          text: `Request Appointment`,
 
-                        // close the sliding item and hide the option buttons
-                        slidingItem.close();
-                    }
-                }
-            ]
-        });
-        // now present the alert on top of all other content
-        alert.present();
-    }
+          handler: () => {
+            window.open('mailto:' + doctor.email);
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
 }
--- src/providers/doctors-data.ts
+++ src/providers/doctors-data.ts
@@ -1,32 +1,49 @@
 /**
  * Created by Madura on 06/10/2016.
  */
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
-import { Http } from '@angular/http';
+
+import {Http} from '@angular/http';
 @Injectable()
 export class DoctorService {
-    data: any;
+  data: any;
 
-    constructor(public http: Http) {}
+  constructor(public http: Http) {
+  }
 
-    load() {
-        if (this.data) {
-            // already loaded data
-            return Promise.resolve(this.data);
-        }
-
-        // don't have the data yet
-        return new Promise(resolve => {
-            // We're using Angular Http provider to request the data,
-            // then on the response it'll map the JSON data to a parsed JS object.
-            // Next we process the data and resolve the promise with the new data.
-            this.http.get('assets/data/doctors.json').subscribe(res => {
-                // we've got back the raw data, now generate the core schedule data
-                // and save the data for later reference
-                this.data = res.json();
-                resolve(this.data);
-            });
-        });
+  load() {
+    if (this.data) {
+      // already loaded data
+      return Promise.resolve(this.data);
     }
+
+    // don't have the data yet
+    return new Promise(resolve => {
+      // We're using Angular Http provider to request the data,
+      // then on the response it'll map the JSON data to a parsed JS object.
+      // Next we process the data and resolve the promise with the new data.
+      this.http.get('assets/data/doctors.json').subscribe(res => {
+        // we've got back the raw data, now generate the core schedule data
+        // and save the data for later reference
+        this.data = res.json();
+        resolve(this.data);
+      });
+    });
+  }
+
+  search(queryText = '') {
+    return this.load().then(data => {
+      console.log("Query text:"+queryText)
+      if (queryText&& queryText !== '') {
+        queryText=queryText.toLowerCase();
+        return data.filter((item) => {
+          return (item.name.toLowerCase().indexOf(queryText) > -1)||(item.hospital.toLowerCase().indexOf(queryText) > -1)||(item.speciality.join().toLowerCase().indexOf(queryText)>-1);
+        })
+      }else{
+        return data;
+      }
+    });
+
+  }
 }
